fb9ab7285715bb33ad478157fd5fa0dcfbeb2f97
SMobeena <shaik.mobeena157@gmail.com>
2022-01-10 00:39:27 +0530

    Two Exception classes added in Exception package "AdminNotFoundException,CustomerNotFoundException", and DriverNotFoundException class implemented

---
+++ cabbookingapplication/src/main/java/com/cg/app/exception/CabNotFoundException.java
@@ -0,0 +1,9 @@
+package com.cg.app.exception;
+
+public class CabNotFoundException extends Exception {
+	
+	public CabNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
--- cabbookingapplication/src/main/java/com/cg/app/exception/DriverNotFoundException.java
+++ cabbookingapplication/src/main/java/com/cg/app/exception/DriverNotFoundException.java
@@ -1,5 +1,8 @@
 package com.cg.app.exception;
 
 public class DriverNotFoundException extends Exception {
-
+	
+	public DriverNotFoundException(String msg) {
+		super(msg);
+	}
 }
