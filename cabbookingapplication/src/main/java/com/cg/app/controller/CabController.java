4bcc27e54a199d184e1341cac750c3f9e074f0d1
Pranav9151 <pranavmahajan9151@gmail.com>
2022-02-02 12:03:25 +0530

    exception

--- cabbookingapplication/src/main/java/com/cg/app/controller/CabController.java
+++ cabbookingapplication/src/main/java/com/cg/app/controller/CabController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.cg.app.exception.CabNotFoundException;
 import com.cg.app.service.ICabService;
 import com.cg.app.service.ICustomerService;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/cab")
 public class CabController {
